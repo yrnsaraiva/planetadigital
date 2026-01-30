51e2ca5d36d5fbca1815c4da12f7c0a363b8249e
Yuran Saraiva <yrnsaraiva03@gmail.com>
2026-01-30 17:36:34 +0200

    update checkout template and cart_items number

--- core/views.py
+++ core/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from shop.models import OrderItem
+from shop.models import OrderItem, Cart
 from events.models import Event
 from django.db.models import Sum
 from django.utils import timezone
+
+
+def buscar_nr_itens_cart(request):
+    try:
+        cart = request.user.cart
+        cart_items = cart.items.select_related(
+            'variant__product'
+        ).all()
+    except Cart.DoesNotExist:
+        cart_items = []
+
+    return cart_items
 
 
 def index(request):
@@ -21,15 +33,22 @@ def index(request):
 
     ctx = {
         'top_product': top_product,
-        'e': evento_mais_proximo
+        'e': evento_mais_proximo,
+        'cart_items': buscar_nr_itens_cart(request)
     }
 
     return render(request, 'core/index.html', context=ctx)
 
 
 def about(request):
-    return render(request, 'core/universe.html')
+
+    ctx = {'cart_items': buscar_nr_itens_cart(request)}
+
+    return render(request, 'core/universe.html', context=ctx)
 
 
 def contact(request):
-    return render(request, 'core/contacto.html')
+
+    ctx = {'cart_items': buscar_nr_itens_cart(request)}
+
+    return render(request, 'core/contacto.html', context=ctx)
